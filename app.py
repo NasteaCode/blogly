8591b4f8a4ab245279b2a6e0eca24143387a430d
whereislima <reach.lima@gmail.com>
2020-10-14 17:31:25 -0700

    implemented html forms and working on app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, request, redirect, render_template
-from models import db, connect_db, Pet
+from models import db, connect_db, User
 
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db, User
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
@@ -13,3 +12,37 @@ app.config['SQLALCHEMY_ECHO'] = True
 
 connect_db(app)
 db.create_all()
+
+# GET /
+@app.route("/")
+def redirect_list_users():
+    
+    return redirect("/users")
+
+
+@app.route("/users")
+def user_listing():
+    
+    return render_template("user_listing.html") 
+
+# GET /users/new   
+@app.route("/users/new")
+def show_add_form():
+
+    return render_template("new_user_form.html")
+
+# POST /users/new
+@app.route("/users/new", methods=["POST"])
+def process_add_form():
+
+    
+    first_name = request.form("first-name")
+    last_name = request.form("last-name")
+    image_url = request.form("image-url")
+
+    new_user = User(first_name, last_name, image_url)
+    db.session.add(user)
+    db.session.commit()
+    
+    return redirect("/users")
+
--- models.py
+++ models.py
@@ -1,5 +1,13 @@
 from flask_sqlalchemy import SQLAlchemy
 
+db = SQLAlchemy()
+
+def connect_db(app):
+    """Connect this database to provided Flask app. You should call this in your Flask app.     """
+    db.app = app
+    db.init_app(app)
+    
+
 """Models for Blogly."""
 class User(db.Model):
     """USER."""
